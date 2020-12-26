eebd3a8ae126fc63a83d6db57c58fe00e6a16ace
Q <coderinch@gmail.com>
2020-12-26 15:24:46 +0900

    move external api into server-side code

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
+require("dotenv").config();
 //import express
 const express = require("express");
 const port = 5000;
+const fetch = require("node-fetch");
 
 // Start up an instance of app
 const app = express();
@@ -20,21 +21,32 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static("website"));
 
+//env variables
+const BASE_URL = process.env.BASE_URL;
+const API_KEY = process.env.API_KEY;
+
 //routes
 // Initialize all route with a callback function
-
 // Callback function to complete GET '/all'
-app.get("/all", (req, res) => {
-  res.json(projectData);
+app.get("/all", async (req, res) => {
+  try {
+    let zip = projectData.zip;
+    let fetchData = await fetch(
+      `${BASE_URL}${zip},us&appid=${API_KEY}`
+    ).then((res) => res.json());
+    res.send(fetchData);
+  } catch (err) {
+    console.log(err);
+  }
 });
+
 // Post Route
 app.post("/postData", (req, res) => {
   projectData = {
     date: req.body.date,
-    temp: req.body.temperature,
     content: req.body.content,
+    zip: req.body.zip,
   };
-  res.send(projectData);
 });
 //server is listening
 app.listen(port, console.log(`server is listening at ${port}`));
